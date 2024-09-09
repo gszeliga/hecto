b74f7b8792351da7ebb6f7f2322b53f0ffdf346a
gszeliga <gszeliga@protonmail.com>
2024-09-09 22:14:00 +0200

    Clear the Screen

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-use std::io::Read;
-use crossterm::terminal::enable_raw_mode;
-use crossterm::terminal::disable_raw_mode;
+#![warn(clippy::all, clippy::pedantic)]
+
+mod editor;
+use editor::Editor;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in std::io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("Binary: {0:08b} ASCII: {0:#03} \n", b);
-                } else {
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?} \n", b, c)
-                }
-        
-                if c == 'q' {
-                    disable_raw_mode().unwrap();
-                    break;
-                }              
-            }
-            Err(err) => print!("Error: {}", err)
-        }
-    }
+    Editor::default().run();
 }
